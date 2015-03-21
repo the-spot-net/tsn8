4ff4f95f85ab866a7edd65ae5ff537115860adbb
[[Neo]] at tsn <neotsn@gmail.com>
2015-03-21 15:46:45 -0500

    Options panel of New Post complete

--- assets/javascript/tsn8.js
+++ assets/javascript/tsn8.js
@@ -1,65 +1,84 @@
 $(document).ready(function () {
-	// Implement jQuery UI Buttons & Button Sets
-	$(function () {
-		$('.buttonset').buttonset();
-		$('[data-mod="tsn8_button"]')
-			.removeClass('button2')
-			.removeClass('button1')
-			.css({'width': 'auto'})
-			.button({
-				text: false
-			});
-		$('.button2, .button1')
-			.removeClass('button2')
-			.removeClass('button1')
-			.css({'width': 'auto'})
-			.button();
-		$('select').selectmenu();
+    // Implement jQuery UI Buttons & Button Sets
+    $(function () {
+        $('[data-mod="tsn8_button"]')
+            .removeClass('button2')
+            .removeClass('button1')
+            .css({'width': 'auto'})
+            .button({
+                text: false
+            });
+        $('.button2, .button1')
+            .removeClass('button2')
+            .removeClass('button1')
+            .css({'width': 'auto'})
+            .button();
 
-		// For posting pages
-		$('#font_select').selectmenu({
-			change: function (event, ui) {
-				var value = $(this).val();
-				bbfontstyle('[size=' + value + ']', '[/size]');
-			}
-		});
+        $('input[data-mod="tsn8_checkbox"]').each(function (ele) {
+            $(this).button({
+                icons: {
+                    primary: ($(this).is(':checked')) ? 'ui-icon-check' : 'ui-icon-blank'
+                },
+                text: true
+            });
 
-		$('#foreground').ColorPicker({
-			onSubmit: function (hsb, hex, rgb, el) {
-				bbfontstyle('[color=#' + hex + ']', '[/color]');
-				$(el).ColorPickerHide();
-			}
-		});
+        }).change(function () {
+            $(this).button("option", {
+                icons: {
+                    primary: (this.checked) ? 'ui-icon-check' : 'ui-icon-blank'
+                },
+                text: true
+            });
+        });
+        $('.buttonset').buttonset();
+        $('select').selectmenu();
 
-		$("#smiley_menu").on('click', function (e) {
-			e.preventDefault();
-			$("#tsn8_newpost_smiley_wrapper").fadeToggle();
-		});
+        // For posting pages
+        $('#font_select').selectmenu({
+            change: function (event, ui) {
+                var value = $(this).val();
+                bbfontstyle('[size=' + value + ']', '[/size]');
+            }
+        });
 
-		$("#tsn8_newpost_smiley_wrapper").on('mouseleave', function () {
-			$(this).fadeOut();
-		});
+        $('#foreground').ColorPicker({
+            onSubmit: function (hsb, hex, rgb, el) {
+                bbfontstyle('[color=#' + hex + ']', '[/color]');
+                $(el).ColorPickerHide();
+            }
+        });
 
-		$('.tsn8_icon_smiley').on('click', function () {
-			var code = $(this).attr('data-code');
-			insert_text(code, true);
-			$('#tsn8_newpost_smiley_wrapper').fadeOut();
-		});
-	});
+        $("#smiley_menu").on('click', function (e) {
+            e.preventDefault();
+            $("#tsn8_newpost_smiley_wrapper").fadeToggle();
+        });
+
+        $("#tsn8_newpost_smiley_wrapper").on('mouseleave', function () {
+            $(this).fadeOut();
+        });
+
+        $('.tsn8_icon_smiley').on('click', function () {
+            var code = $(this).attr('data-code');
+            insert_text(code, true);
+            $('#tsn8_newpost_smiley_wrapper').fadeOut();
+        });
+
+
+    });
 });
 
 function getUrlParameter(sParam) {
-	var sPageURL = window.location.search.substring(1);
-	var sURLVariables = sPageURL.split('&');
-	var result = null;
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    var result = null;
 
-	for (var i = 0; i < sURLVariables.length; i++) {
-		var sParameterName = sURLVariables[i].split('=');
-		if (sParameterName[0] == sParam) {
-			result = sParameterName[1];
-			break;
-		}
-	}
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            result = sParameterName[1];
+            break;
+        }
+    }
 
-	return result;
+    return result;
 }
