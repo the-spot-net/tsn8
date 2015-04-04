b98e9fcfb366c46b00d2678a85720bbcf5d515fb
[[Neo]] at tsn <neotsn@gmail.com>
2015-04-04 10:38:48 -0500

    Updates to members list, team list, css; fixes to avatar gallery dropdown & other settings dropdowns; addition of avatars from punix forums.

--- assets/javascript/tsn8.js
+++ assets/javascript/tsn8.js
@@ -1,84 +1,96 @@
 $(document).ready(function () {
-    // Implement jQuery UI Buttons & Button Sets
-    $(function () {
-        $('[data-mod="tsn8_button"]')
-            .removeClass('button2')
-            .removeClass('button1')
-            .css({'width': 'auto'})
-            .button({
-                text: false
-            });
-        $('.button2, .button1')
-            .removeClass('button2')
-            .removeClass('button1')
-            .css({'width': 'auto'})
-            .button();
+	// Implement jQuery UI Buttons & Button Sets
+	$(function () {
+		$('[data-mod="tsn8_button"]')
+			.removeClass('button2')
+			.removeClass('button1')
+			.css({'width': 'auto'})
+			.button({
+				text: false
+			});
+		$('.button2, .button1')
+			.removeClass('button2')
+			.removeClass('button1')
+			.css({'width': 'auto'})
+			.button();
 
-        $('input[data-mod="tsn8_checkbox"]').each(function (ele) {
-            $(this).button({
-                icons: {
-                    primary: ($(this).is(':checked')) ? 'ui-icon-check' : 'ui-icon-blank'
-                },
-                text: true
-            });
+		$('input[data-mod="tsn8_checkbox"]').each(function (ele) {
+			$(this).button({
+				icons: {
+					primary: ($(this).is(':checked')) ? 'ui-icon-check' : 'ui-icon-blank'
+				},
+				text: true
+			});
 
-        }).change(function () {
-            $(this).button("option", {
-                icons: {
-                    primary: (this.checked) ? 'ui-icon-check' : 'ui-icon-blank'
-                },
-                text: true
-            });
-        });
-        $('.buttonset').buttonset();
-        $('select').selectmenu();
+		}).change(function () {
+			$(this).button("option", {
+				icons: {
+					primary: (this.checked) ? 'ui-icon-check' : 'ui-icon-blank'
+				},
+				text: true
+			});
+		});
+		$('.buttonset').buttonset();
 
-        // For posting pages
-        $('#font_select').selectmenu({
-            change: function (event, ui) {
-                var value = $(this).val();
-                bbfontstyle('[size=' + value + ']', '[/size]');
-            }
-        });
+		$('select').selectmenu({
+			change: function () {
+				var sourceElementId = $(this).attr('id').replace('-button', ''),
+					$source = $('#' + sourceElementId);
 
-        $('#foreground').ColorPicker({
-            onSubmit: function (hsb, hex, rgb, el) {
-                bbfontstyle('[color=#' + hex + ']', '[/color]');
-                $(el).ColorPickerHide();
-            }
-        });
+				$source.val($(this).val()); // Update the source selection
+				$source.children().each(function () {
+					var $this = $(this),
+						$setting = $($this.data('toggle-setting'));
+					$setting.toggle($this.is(':selected'));
+				});
+			}
+		});
 
-        $("#smiley_menu").on('click', function (e) {
-            e.preventDefault();
-            $("#tsn8_newpost_smiley_wrapper").fadeToggle();
-        });
+		// For posting pages
+		$('#font_select').selectmenu({
+			change: function (event, ui) {
+				var value = $(this).val();
+				bbfontstyle('[size=' + value + ']', '[/size]');
+			}
+		});
 
-        $("#tsn8_newpost_smiley_wrapper").on('mouseleave', function () {
-            $(this).fadeOut();
-        });
+		$('#foreground').ColorPicker({
+			onSubmit: function (hsb, hex, rgb, el) {
+				bbfontstyle('[color=#' + hex + ']', '[/color]');
+				$(el).ColorPickerHide();
+			}
+		});
 
-        $('.tsn8_icon_smiley').on('click', function () {
-            var code = $(this).attr('data-code');
-            insert_text(code, true);
-            $('#tsn8_newpost_smiley_wrapper').fadeOut();
-        });
+		$("#smiley_menu").on('click', function (e) {
+			e.preventDefault();
+			$("#tsn8_newpost_smiley_wrapper").fadeToggle();
+		});
 
+		$("#tsn8_newpost_smiley_wrapper").on('mouseleave', function () {
+			$(this).fadeOut();
+		});
 
-    });
+		$('.tsn8_icon_smiley').on('click', function () {
+			var code = $(this).attr('data-code');
+			insert_text(code, true);
+			$('#tsn8_newpost_smiley_wrapper').fadeOut();
+		});
+
+	});
 });
 
 function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    var result = null;
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	var result = null;
 
-    for (var i = 0; i < sURLVariables.length; i++) {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) {
-            result = sParameterName[1];
-            break;
-        }
-    }
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) {
+			result = sParameterName[1];
+			break;
+		}
+	}
 
-    return result;
+	return result;
 }
---
+++ images/avatars/gallery/FF7ACAvatars/Readme.txt
@@ -0,0 +1,8 @@
+Here are 38 Final Fantasy 7 Advent Children Avatars. 
+feel free to upload them to your forums and put them up for download just make sure you include this readme file and give me credit for making them.
+Released: 03/03/05
+
+Author: Dark Rider
+Website: http://www.quickscripts.info
+Email: endofsorrow1990@gmail.com
+Feel free to email or post on my forums if you want me to make any more avatars or if you just want to chat.
