a0d25a7b938075680f221449090b68d22c1456d6
@neotsn <neotsn@gmail.com>
2015-11-29 12:36:09 -0600

    Update to phpBB 3.1.16

--- includes/functions_compatibility.php
+++ includes/functions_compatibility.php
@@ -20,20 +20,21 @@ if (!defined('IN_PHPBB'))
 }
 
 /**
- * Get user avatar
- *
- * @deprecated 3.1.0-a1 (To be removed: 3.3.0)
- *
+* Get user avatar
+*
+* @deprecated 3.1.0-a1 (To be removed: 3.3.0)
+*
  * @param string $avatar        Users assigned avatar name
  * @param int    $avatar_type   Type of avatar
  * @param string $avatar_width  Width of users avatar
- * @param string $avatar_height Height of users avatar
- * @param string $alt           Optional language string for alt tag within image, can be a language key or text
- * @param bool   $ignore_config Ignores the config-setting, to be still able to view the avatar in the UCP
- *
- * @return string Avatar image
- */
-function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $alt = 'USER_AVATAR', $ignore_config = false)
+* @param string $avatar_height Height of users avatar
+* @param string $alt Optional language string for alt tag within image, can be a language key or text
+* @param bool $ignore_config Ignores the config-setting, to be still able to view the avatar in the UCP
+* @param bool $lazy If true, will be lazy loaded (requires JS)
+*
+* @return string Avatar image
+*/
+function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $alt = 'USER_AVATAR', $ignore_config = false, $lazy = false)
 {
 	// map arguments to new function phpbb_get_avatar()
 	$row = array(
@@ -43,7 +44,7 @@ function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $
 		'avatar_height'	=> $avatar_height,
 	);
 
-	return phpbb_get_avatar($row, $alt, $ignore_config);
+	return phpbb_get_avatar($row, $alt, $ignore_config, $lazy);
 }
 
 /**
